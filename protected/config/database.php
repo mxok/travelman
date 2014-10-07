ad8331897d041125d447429133de03db4c1dfdb2
safsafsa <xujianxing@manyouren.com>
2014-10-07 14:32:21 +0800

    fix bug

--- protected/config/database.php
+++ protected/config/database.php
@@ -1,15 +1,6 @@
 <?php
 
 return
- // array(
- //             'connectionString' => 'mysql:host=211.154.6.229;port=3307;dbname=travelman',
- //             'emulatePrepare' => true,
- //             'username' => 'travelman',
- //             'password' => 'travelman',
- //             'charset' => 'utf8',
- //             'tablePrefix' => 'travel_',
- //             'nullConversion'=>PDO::NULL_TO_STRING
- //         ) ;
 array(
    'connectionString' => 'mysql:host=localhost;port=3306;dbname=manyouren',
    'emulatePrepare' => true,
@@ -19,17 +10,5 @@ array(
    'tablePrefix' => 'travel_',
    'nullConversion'=>PDO::NULL_TO_STRING
 );
-//
-//
-//
-//
- // array(
- //             'connectionString' => 'mysql:host=manyouren.mysql.rds.aliyuncs.com;port=3306;dbname=masterdb',
- //             'emulatePrepare' => true,
- //             'username' => 'manyouren',
- //             'password' => 'manyouren',
- //             'charset' => 'utf8',
- //             'tablePrefix' => 'travel_',
- //             'nullConversion'=>PDO::NULL_TO_STRING
- //         ) ;
+
 ?>
